b40069174c495820fb37acd51f160cb762a12e80
Nathan <70660308+NotARoomba@users.noreply.github.com>
2025-08-04 17:02:07 -0500

    4040 page an seo

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   build: {
     outDir: '../docs',
-    emptyOutDir: true,  
+    emptyOutDir: true,
   },
 })
